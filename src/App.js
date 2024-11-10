1be3786b5f74bc532d3c539b336380f050ec1890
Doddanna <doddannab17@gmail.com>
2024-11-10 13:05:01 +0530

    Updating book with variable - JSX Javascript

--- src/App.js
+++ src/App.js
@@ -20,14 +20,15 @@ const BookList = () => {
 };
 
 const Book = () => {
+  const imageUrl =
+    "https://images-eu.ssl-images-amazon.com/images/I/81rjyBm+UYL._AC_UL900_SR900,600_.jpg";
+  const author = "Ravi Mantri";
+  const bookTitle = "Amma Diarylo Konni Pageelu";
   return (
     <div className="book">
-      <img
-        src="https://images-eu.ssl-images-amazon.com/images/I/81rjyBm+UYL._AC_UL900_SR900,600_.jpg"
-        alt="Amma Diarylo Konni Pageelu"
-      />
-      <h2>Amma Diarylo Konni Pageelu</h2>
-      <h4>Ravi Mantri</h4>
+      <img src={imageUrl} alt={bookTitle} />
+      <h2>{bookTitle}</h2>
+      <h4>{author}</h4>
     </div>
   );
 };
