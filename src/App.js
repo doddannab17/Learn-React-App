a52fc574a8487a0dfdb4b7e9f4f17022c85e1f64
Doddanna <doddannab17@gmail.com>
2024-11-10 12:03:51 +0530

    Nested component
    Testing nested component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SubComponent />
+        <SubComponent2 />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -20,6 +22,14 @@ function App() {
       </header>
     </div>
   );
+}
+
+function SubComponent() {
+  return <h1>Hello Component 1</h1>;
+}
+
+function SubComponent2() {
+  return <h1>Hello Component 2</h1>;
 }
 
 export default App;
